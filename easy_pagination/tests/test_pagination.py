6b9e2e058ec5d64d61362522e7736d0a44dd09fb
cassymyo-spec <cassymyo@gmail.com>
2025-12-25 14:53:26 +0200

    update test

--- easy_pagination/tests/test_pagination.py
+++ easy_pagination/tests/test_pagination.py
@@ -16,40 +16,7 @@ from django.test import TestCase
 from rest_framework.test import APIRequestFactory
 from rest_framework.request import Request
 
-# Add the parent directory to the Python path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
-
-# Configure minimal Django settings
-if not settings.configured:
-    settings.configure(
-        DATABASES={
-            'default': {
-                'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': ':memory:'
-            }
-        },
-        INSTALLED_APPS=[
-            'django.contrib.contenttypes',
-            'django.contrib.auth',
-            'rest_framework',
-        ],
-        REST_FRAMEWORK={
-            'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-            'PAGE_SIZE': 10
-        },
-        SECRET_KEY='test-key'
-    )
-    import django
-    django.setup()
-
-# Import your pagination classes
-from easy_pagination import (
-    StandardPagination,
-    LargeResultsPagination,
-    SmallResultsPagination,
-    NoPagination,
-    get_pagination_class
-)
 
 from .. import (
     StandardPagination,
